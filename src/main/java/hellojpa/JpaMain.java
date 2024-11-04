174f0029d24ff742a35235b90e6efa45b1a08924
eehyeonzee <eehyeonzee@gmail.com>
2024-11-04 18:15:19 +0900

    TIL_DAY3 >> 4. 영속성 관리 (내부 동작 방식)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -41,15 +41,36 @@ public class JpaMain {
             // 삭제
 //            em.remove(findMember);
             // jpa 입장에서는 table이 아닌 엔티티 객체를 대상으로 함 (JPQL)
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5) // 이런 식으로 limit이나 rownum 설정 할 수 있음
-                    .setMaxResults(8)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5) // 이런 식으로 limit이나 rownum 설정할 수 있음
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            Member member = new Member(3L, "HelloC");
+//            em.persist(member); // 영속성 컨텍스트에 엔티티가 담기는 시점
+            /*
+            * flush
+            * 1) flush 직접 호출
+            * 2) 미리 db에 반영되는 게 보고 싶거나 쿼리가 어떻게 날아가는지 보고 싶을 때 확인 가능
+            * 3) 1차캐시가 리셋되거나 하지 않음 (전혀 상관없음)
+            * 4) 플러시는 오직 영속성 컨텍스 내 쓰기지연SQL저장소에 쌓여있는 쿼리문들이 DB에 저장되는 과정일 뿐임
+            * */
+//            em.flush();
 
+            // 영속 -> 준영속
+            Member member = em.find(Member.class, 3L);
+            member.setName("HelloCCC"); // 1) 변경값을 세팅해도
+            // [detach] 특정 엔티티만 준영속 상태로 전환
+//            em.detach(member); // 2-1) 영속성 상태의 엔티티를 영속성 컨텍스트로부터 분리시킴(detached)
+            // [clear] 영속성 컨텍스트를 완전히 초기화
+//            em.clear(); // 2-2) 1차 캐시 통으로 지움 >> clear 이후에 다시 find로 조회 시, 완전히 새로운 영속성 컨텍스트가 올라가고 select 쿼리가 돎
+            // 3) JPA가 더이상 해당 엔티티를 관리하지 않음
+            System.out.println("==============================");
+            // 4) 결론적으로 find(select) 쿼리만 돌고 set(update) 쿼리는 돌지 않음
             // commit
             tx.commit();
         } catch (Exception e) {
