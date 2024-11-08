985580f142bc24143c1a019677a848dd607c9d30
eehyeonzee <eehyeonzee@gmail.com>
2024-11-09 00:59:00 +0900

    TIL_DAY5 >> 5. 엔티티 매핑 >> 1) 데이터베이스 스키마 자동 생성 >> 2) 필드와 컬럼 매핑 >> 3) 기본키 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -27,31 +27,31 @@ public class JpaMain {
 
         try {
             // 데이터 생성
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
+            /*Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");*/
             // jpa에 저장
-//            em.persist(member);
+            // em.persist(member);
             // 조회
-//            Member findMember = em.find(Member.class, 1L); // EntityManager는 마치 자바 컬렉션처럼 생각하면 됨 >> 객체를 대신 저장해주는 놈
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
+            /*Member findMember = em.find(Member.class, 1L); // EntityManager는 마치 자바 컬렉션처럼 생각하면 됨 >> 객체를 대신 저장해주는 놈
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());*/
             // 수정
-//            findMember.setName("HelloJPA");
+            // findMember.setName("HelloJPA");
             // 삭제
-//            em.remove(findMember);
+            // em.remove(findMember);
             // jpa 입장에서는 table이 아닌 엔티티 객체를 대상으로 함 (JPQL)
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(5) // 이런 식으로 limit이나 rownum 설정할 수 있음
-//                    .setMaxResults(8)
-//                    .getResultList();
-//
-//            for (Member member : result) {
-//                System.out.println("member.name = " + member.getName());
-//            }
+            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5) // 이런 식으로 limit이나 rownum 설정할 수 있음
+                    .setMaxResults(8)
+                    .getResultList();
 
-//            Member member = new Member(3L, "HelloC");
-//            em.persist(member); // 영속성 컨텍스트에 엔티티가 담기는 시점
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            Member member = new Member(3L, "HelloC");
+            em.persist(member); // 영속성 컨텍스트에 엔티티가 담기는 시점*/
             /*
             * flush
             * 1) flush 직접 호출
@@ -59,19 +59,43 @@ public class JpaMain {
             * 3) 1차캐시가 리셋되거나 하지 않음 (전혀 상관없음)
             * 4) 플러시는 오직 영속성 컨텍스 내 쓰기지연SQL저장소에 쌓여있는 쿼리문들이 DB에 저장되는 과정일 뿐임
             * */
-//            em.flush();
+            // em.flush();
 
             // 영속 -> 준영속
-            Member member = em.find(Member.class, 3L);
-            member.setName("HelloCCC"); // 1) 변경값을 세팅해도
+            /* Member member = em.find(Member.class, 3L);
+            member.setUsername("HelloCCC"); // 1) 변경값을 세팅해도
             // [detach] 특정 엔티티만 준영속 상태로 전환
-//            em.detach(member); // 2-1) 영속성 상태의 엔티티를 영속성 컨텍스트로부터 분리시킴(detached)
+            em.detach(member); // 2-1) 영속성 상태의 엔티티를 영속성 컨텍스트로부터 분리시킴(detached)
             // [clear] 영속성 컨텍스트를 완전히 초기화
             em.clear(); // 2-2) 1차 캐시 통으로 지움 >> clear 이후에 다시 find로 조회 시, 완전히 새로운 영속성 컨텍스트가 올라가고 select 쿼리가 돎
-//            Member member2 = em.find(Member.class, 3L);
+            Member member2 = em.find(Member.class, 3L);
             // 3) JPA가 더이상 해당 엔티티를 관리하지 않음
             System.out.println("==============================");
-            // 4) 결론적으로 find(select) 쿼리만 돌고 set(update) 쿼리는 돌지 않음
+            // 4) 결론적으로 find(select) 쿼리만 돌고 set(update) 쿼리는 돌지 않음*/
+
+            // 엔티티 매핑
+            Member member1 = new Member();
+//            member.setId("ID_A");
+            member1.setUsername("A");
+
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("=====================");
+
+            em.persist(member1); // 1, 51 (미리 51에서 100까지 확보해야 하므로 call next for member_seq 한번 더 호출 >> 총 2번 호출)
+            em.persist(member2); // MEM
+            em.persist(member3); // MEM
+
+            System.out.println("member1.getId() = " + member1.getId());
+            System.out.println("member2.getId() = " + member2.getId());
+            System.out.println("member3.getId() = " + member3.getId());
+
+            System.out.println("=====================");
+
             // commit
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,22 +1,75 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+//@TableGenerator(name = "member_seq_generator", table = "my_sequences", pkColumnValue = "member_seq", allocationSize = 1)
+//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+@SequenceGenerator(name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50) // 1부터 시작하고, 1씩 증가시켜라
 //@Table(name = "MBR")
 public class Member {
 
-    @Id
+    @Id // 직접할당
+//    @GeneratedValue(strategy = GenerationType.AUTO) // AUTO: DB 방언에 맞춰 자동 생성
+      /*
+      * IDENTITY: DB에 값이 들어가야만 ID값을 알 수 있음 (단점)
+      * 다시 말해, DB에 insert되기 전까지 id값을 모름
+      * jpa 입장에서는 영속성 컨텍스트 내 1차캐시에 엔티티를 저장하려면 pk값이 필요한데 방법이 없음
+      * 그래서 이 경우에만 commit 전, em.persist(~)를 호출하는 시점에 쿼리를 날림
+      * 그 후 내부적으로 pk값을 가져와서 영속성 컨텍스트의 pk값으로 사용함
+      * => sql문장을 모았다가(buffering) 한꺼번에 commit하는 것이 IDENTITY 전략에서는 불가능
+      * */
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+      /*
+      * SEQUENCE: 유일한 값을 순서대로 생성하는 특별한 데이터베이스 오브젝트 (ex. oracle sequence)
+      * 시퀀스 오브젝트는 DB에서 시퀀스 값을 애플리케이션으로 가져옴
+      * buffering 방식 가능 >> 한번에 모았다가 commit 시점에 insert쿼리 돎
+      * */
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "member_seq_generator")
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    /*private Integer age;
+
+    @Enumerated(EnumType.STRING) // EnumType.ORDINAL은 사용x >> 별도의 데이터 타입이 추가되면 순서에 의한 문제 발생할 수 있기 때문
+    private RoleType roleType;
+
+    // 과거
+    @Temporal(TemporalType.TIMESTAMP) // java8 이후로 잘 사용하지 않음
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // 최신
+    private LocalDate testLcDt; // date
+    private LocalDateTime testLcDtTm; // timestamp
+
+    @Lob // 매핑하는 필드 타입이 문자이면 CLOB, 나머지는 BLOB 매핑
+    private String description;
+
+    @Transient // 메모리 상에서만 필드를 사용하고 싶을 때
+    private int temp;*/
 
     public Member() {}
-    public Member(Long id, String name) {
+
+    /*public Member(Long id, String username, Integer age, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
         this.id = id;
-        this.name = name;
-    }
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.description = description;
+    }*/
 
     public Long getId() {
         return id;
@@ -26,11 +79,53 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+    /*public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }*/
 }
+
+
